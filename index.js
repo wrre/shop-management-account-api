cd171e04d4e5b9ebb276813246e8fe955299fe36
Peggy Liu <fnoihtlanl@gmail.com>
2021-12-09 22:38:39 +0800

    setup eslint and prettier

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/health', function (req, res) {
-    res.send('ok')
-})
+app.get('/health', (req, res) => {
+  res.send('ok');
+});
 
 app.listen(3001);
