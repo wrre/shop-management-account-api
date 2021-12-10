cc5519792dd8a8a19237024ff0cd134d9cf20ed0
Peggy Liu <fnoihtlanl@gmail.com>
2021-12-10 17:20:47 +0800

    add getLoginByLineUrl and loginByLine service function

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,27 +1,11 @@
 import { Router } from 'express';
-import * as querystring from 'querystring';
-import axios from 'axios';
-import { AccountModel } from '../models';
 import { AccountService } from '../services';
 
 export const authRouter = Router();
 
-const { CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE, SCOPE, CLIENT_SECRET } =
-  process.env;
-
-const state = 'shop-management';
-const nonce = '09876xyz';
-
 authRouter.get('/logins/line/url', (req, res) => {
-  const query = querystring.stringify({
-    response_type: RESPONSE_TYPE,
-    client_id: CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
-    state,
-    scope: SCOPE,
-    nonce,
-  });
-  res.send({ url: `https://access.line.me/oauth2/v2.1/authorize?${query}` });
+  const url = AccountService.getLoginByLineUrl();
+  res.send({ url });
 });
 
 authRouter.get('/logins/line/callback', async (req, res) => {
@@ -31,60 +15,10 @@ authRouter.get('/logins/line/callback', async (req, res) => {
 authRouter.post('/logins/line', async (req, res) => {
   console.log(`POST /logins/line, body:`, req.body);
 
-  const { code, state } = req.body;
-
-  const verifyCodeResponse = await axios
-    .post(
-      'https://api.line.me/oauth2/v2.1/token',
-      querystring.stringify({
-        grant_type: 'authorization_code',
-        code,
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        redirect_uri: REDIRECT_URI,
-      }),
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
-    )
-    .then(({ data }) => data)
-    .catch((e) => {
-      console.log('[ERROR] login by line verify code error', e.response.data);
-      res.sendStatus(403);
-    });
-
-  if (!verifyCodeResponse) {
-    return;
+  try {
+    const account = await AccountService.loginByLine(req.body);
+    res.json(account);
+  } catch (e) {
+    res.sendStatus(403);
   }
-
-  const verifyTokenResponse = await axios
-    .post(
-      'https://api.line.me/oauth2/v2.1/verify',
-      querystring.stringify({
-        id_token: verifyCodeResponse.id_token,
-        client_id: CLIENT_ID,
-      }),
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
-    )
-    .then(({ data }) => data)
-    .catch((e) => {
-      console.log('[ERROR] login by line verify token error', e.response.data);
-
-      res.sendStatus(500);
-    });
-
-  if (!verifyTokenResponse) {
-    return;
-  }
-
-  const { sub: lineThirdPartyId, name } = verifyTokenResponse;
-
-  const account = await AccountService.findOrCreateAccount({
-    name,
-    lineThirdPartyId,
-  });
-
-  console.log(
-    `POST /logins/line, id: ${account.id}, name: ${name}, lineThirdPartyId: ${lineThirdPartyId}`,
-  );
-
-  res.json(account);
 });
--- src/services/account.service.js
+++ src/services/account.service.js
@@ -1,7 +1,27 @@
+import querystring from 'querystring';
+import axios from 'axios';
 import { AccountModel } from '../models';
 import { ROLE_MAP } from '../common/role.map';
 
+const { CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE, SCOPE, CLIENT_SECRET } =
+  process.env;
+
+const state = 'shop-management';
+const nonce = '09876xyz';
+
 export class AccountService {
+  static getLoginByLineUrl() {
+    const query = querystring.stringify({
+      response_type: RESPONSE_TYPE,
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      state,
+      scope: SCOPE,
+      nonce,
+    });
+    return `https://access.line.me/oauth2/v2.1/authorize?${query}`;
+  }
+
   static async findOrCreateAccount(data) {
     const { name, ...login } = data;
     let account = await AccountModel.findOne({
@@ -16,6 +36,61 @@ export class AccountService {
         ...login,
       });
     }
+
+    return account;
+  }
+
+  static async loginByLine(data) {
+    const { code, state } = data;
+
+    const verifyCodeResponse = await axios
+      .post(
+        'https://api.line.me/oauth2/v2.1/token',
+        querystring.stringify({
+          grant_type: 'authorization_code',
+          code,
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          redirect_uri: REDIRECT_URI,
+        }),
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
+      )
+      .then((response) => response.data)
+      .catch((e) => {
+        console.log('[ERROR] loginByLine verify code error', e.response.data);
+
+        throw e.data;
+      });
+
+    const verifyTokenResponse = await axios
+      .post(
+        'https://api.line.me/oauth2/v2.1/verify',
+        querystring.stringify({
+          id_token: verifyCodeResponse.id_token,
+          client_id: CLIENT_ID,
+        }),
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
+      )
+      .then((response) => response.data)
+      .catch((e) => {
+        console.error(
+          '[ERROR] loginByLine verify token error',
+          e.response.data,
+        );
+
+        throw e.data;
+      });
+
+    const { sub: lineThirdPartyId, name } = verifyTokenResponse;
+
+    const account = await this.findOrCreateAccount({
+      name,
+      lineThirdPartyId,
+    });
+
+    console.log(
+      `loginByLine, id: ${account.id}, name: ${name}, lineThirdPartyId: ${lineThirdPartyId}`,
+    );
 
     return account;
   }
